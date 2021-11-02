ee7f811ef6b3b4dee5942fb5a5e9b8369c748127
p22rlugo <p22rlugo@fairfieldprep.org>
2021-11-02 10:20:53 -0400

    Add file

---
+++ hw5-4.py
@@ -0,0 +1,30 @@
+# Ryan Lugo: RJL 11/2/21
+
+# Question 1
+print("Python"[2])
+
+# Question 2
+print("Strings are simply a seuquence of characters."[31])
+
+# Question 3
+print(len("excellent"),"is the length")
+
+# Question 4
+print("apple" in "Pineapple")
+
+# Question 5
+print()
+
+# Question 6
+print("x" * 100)
+
+# Question 7
+print()
+
+# Question 8
+print()
+
+# Question 9
+print()
+
+# Question 10
